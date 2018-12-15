9868325aa33cb307620a09ab7a2b792f6baece31
lorix <foxred324@gmail.com>
2018-12-15 18:28:31 +0200

    redirect routing

--- src/Controller/TestsController.php
+++ src/Controller/TestsController.php
@@ -3,10 +3,7 @@ namespace App\Controller;
 use App\Controller\AppController;
 
 class TestsController extends AppController{
-    public function index(){
-
-    }
-    public function args($arg1,$arg2){
+    public function index($arg1,$arg2){
         $this->set('argument1',$arg1);
         $this->set('argument2',$arg2);
     }
