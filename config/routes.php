845aab49cf31fc3e5aee0c1f8815c53c71644e70
lorix <foxred324@gmail.com>
2018-12-16 15:08:10 +0200

    redirect routing

--- config/routes.php
+++ config/routes.php
@@ -12,11 +12,9 @@ Router::scope('/', function (RouteBuilder $routes) {
     $routes->connect('tests/:arg1/:arg2', ['controller' => 'Tests', 'action'=>
         'args'],['pass' =>['arg1', 'arg2']]);
 
-    $routes->connect('/redirectcontroller',['
-         controller'=>'Redirects','action'=>'action1']);
+    $routes->connect('/redirectcontroller',['controller'=>'Redirects','action'=>'action1']);
 
-    $routes->connect('/redirectcontroller2',['
-         controller'=>'Redirects','action'=>'action2']);
+    $routes->connect('/redirectcontroller2',['controller'=>'Redirects','action'=>'action2']);
 
     $routes->fallbacks('DashedRoute');
 
