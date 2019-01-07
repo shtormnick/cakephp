1e97665e10c315389d512259fd33d2b8383128a0
lorix <foxred324@gmail.com>
2019-01-07 18:43:31 +0200

    events 1.2

--- src/Controller/TestsController.php
+++ src/Controller/TestsController.php
@@ -5,6 +5,16 @@ use App\Controller\AppController;
 class TestsController extends AppController{
     public function index(){
 
+        $event=$this->getEventManager();
+        $event->on(
+            'Controller.beforeRender',
+            function ($event){
+                var_dump($event->subject()->viewVars);
+                die();
+            }
+        );
+        var_dump($event);
+        $this->set('argument1','ddddd');
     }
     public function args($arg1,$arg2){
         $this->set('argument1',$arg1);
