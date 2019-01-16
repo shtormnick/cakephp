ed2a8521d0f80cb005b5d6bc0afc9118708bb654
lorix <foxred324@gmail.com>
2019-01-16 20:37:30 +0200

    log in fixed
    (cherry picked from commit 3b52ad2)

--- config/app.php
+++ config/app.php
@@ -255,9 +255,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
+            'username' => 'homestead',
             'password' => 'secret',
-            'database' => 'my_app',
+            'database' => 'homestead',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
