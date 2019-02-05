f909cd98d8192ef816c95edc1003aa17e772145a
lorix <foxred324@gmail.com>
2019-02-05 16:32:23 +0200

    session management

--- config/routes.php
+++ config/routes.php
@@ -18,6 +18,19 @@ Router::scope('/', function (RouteBuilder $routes) {
     $routes->connect('/redirectcontroller2',['
          controller'=>'Redirects','action'=>'action2']);
 
+    $routes->connect('/sessionobject',
+        ['controller'=>'Sessions','action'=>'index']);
+    $routes->connect('/sessionread',
+        ['controller'=>'Sessions','action'=>'retrieve_session_data']);
+    $routes->connect('/sessionwrite',
+        ['controller'=>'Sessions','action'=>'write_session_data']);
+    $routes->connect('/sessioncheck',
+        ['controller'=>'Sessions','action'=>'check_session_data']);
+    $routes->connect('/sessiondelete',
+        ['controller'=>'Sessions','action'=>'delete_session_data']);
+    $routes->connect('/sessiondestroy',
+        ['controller'=>'Sessions','action'=>'destroy_session_data']);
+
     $routes->fallbacks('DashedRoute');
 
 });
---
+++ src/Template/Sessions/write_session_data.ctp
@@ -0,0 +1,1 @@
+The data has been written in session.
